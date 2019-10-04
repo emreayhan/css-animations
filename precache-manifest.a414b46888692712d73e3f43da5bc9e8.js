9e4f44655ac2ca681f16276004a15e24c47efa44
emre.ayhan <emre.ayhan@kgteknoloji.com>
2019-10-04 09:56:41 +0300

    Updates

---
+++ precache-manifest.a414b46888692712d73e3f43da5bc9e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cd8f1093edcb5064c8ee3a108a4e3ef",
+    "url": "/css-animations/index.html"
+  },
+  {
+    "revision": "51eeefd0efb63c655252",
+    "url": "/css-animations/static/css/main.15b8afda.chunk.css"
+  },
+  {
+    "revision": "7ffa368ae5e37ee7f7ff",
+    "url": "/css-animations/static/js/2.f3c72563.chunk.js"
+  },
+  {
+    "revision": "51eeefd0efb63c655252",
+    "url": "/css-animations/static/js/main.4c3590b6.chunk.js"
+  },
+  {
+    "revision": "976f3c785af26b346062",
+    "url": "/css-animations/static/js/runtime-main.8a9f9da0.js"
+  },
+  {
+    "revision": "d3783f40cd0ae9368fa6da097425b72e",
+    "url": "/css-animations/static/media/image2.d3783f40.jpeg"
+  }
+]);
